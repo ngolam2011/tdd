34abc624b603bebe739fe0850d5403b2b2fff782
Ngo Lam <lngo9@csc.com>
2018-02-06 18:51:57 +0700

    initial

---
+++ src/main/java/Dollar.java
@@ -0,0 +1,12 @@
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
---
+++ src/test/java/MultiCurrencyTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MultiCurrencyTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+    @Test
+    public void testEquality() {
+       assertTrue(new Dollar(5).equals(new Dollar(5)));
+    }
+}
